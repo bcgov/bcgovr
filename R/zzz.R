5278a4586365bd315a89d4c240d81cdca4f056af
Andy Teucher <andy.teucher@gmail.com>
2015-01-08 10:03:05 -0800

    Tweak setting ghostscript environment variable

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,23 @@
 .onLoad <- function(libname, pkgname) {
-  set_ghostscript()
-}
-
-set_ghostscript <- function(gsfile = "C:/Program Files/gs/gs9.15/bin/gswin64c.exe") {
-  if (file.exists(gsfile)) {
-    Sys.setenv(R_GSCMD=gsfile)
-  } else {
+  require("extrafont")
+  if (!set_ghostscript()) {
     packageStartupMessage("You do not appear to have ghostscript installed. ",
                           "Without it you will not be able to embed fonts in pdfs. ", 
-                          "Please install it from ", 
+                          "If you require this, please install ghostscript from ", 
                           "'http://ghostscript.com/download/gsdnld.html' and ",
                           "run envreportutils:::set_ghostscript('path_to_executable')")
   }
+}
+
+#' Set the environment variable for the path to the ghostscript executable.
+#' @param gsfile path to the ghostscript executable
+#' @keywords internal
+#' @return logical dependent on whether or not the file exists
+set_ghostscript <- function(gsfile = "C:/Program Files/gs/gs9.15/bin/gswin64c.exe") {
+  ret <- FALSE
+  if (file.exists(gsfile)) {
+    Sys.setenv(R_GSCMD=gsfile)
+    ret <- TRUE
+  }
+  ret
 }
