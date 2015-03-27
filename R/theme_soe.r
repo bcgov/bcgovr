a8650ce915ab53a6fd1d19670f00d594433d35e7
Andy Teucher <andy.teucher@gov.bc.ca>
2015-03-27 13:14:42 -0700

    Remove theme_foundation, import ggthemes

--- R/theme_soe.r
+++ R/theme_soe.r
@@ -1,21 +1,50 @@
 #' Default theme for EnvReportBC graphs and plots
-#'
 #' 
-#' 
-#' @import ggplot2
+#' @import ggplot2 ggthemes
 #' @param  base_size base font size (default = 12)
 #' @param  base_family base font family (default = Verdana)
 #' @param  use_sizes use relative font sizes (?)
-#' @param  facet facetting? (advise to not use directly, is set by \code{theme_soe_facet})
 #' @export
 #' @keywords plotting theme
 #' @return returns a plot theme
 #' @examples \dontrun{
 #' 
 #'}
-theme_soe <- function(base_size=12, base_family="Verdana", use_sizes=TRUE, facet = FALSE) {
-  thm <- theme_foundation_null(base_size = base_size, 
-                               base_family = base_family) +
+theme_soe <- function(base_size=12, base_family="Verdana", use_sizes=TRUE) {
+  thm <- theme_soe_foundation(base_size = base_size, base_family = base_family, 
+                       use_sizes = use_sizes)
+  thm
+}  
+
+#' Soe plot theme for facetted graphs
+#' 
+#' @import ggplot2 ggthemes
+#' @param  base_size base font size (default = 12)
+#' @param  base_family base font family (default = Verdana)
+#' @param  use_sizes use relative font sizes (?)
+#' @export
+#' @keywords plotting theme
+#' @return a ggplot2 theme
+#' @examples \dontrun{
+#' 
+#'}
+theme_soe_facet <- function(base_size=12, base_family="Verdana", use_sizes=TRUE) {
+  
+  theme_soe_foundation(base_size = base_size, base_family = base_family, 
+                       use_sizes = use_sizes) + 
+    theme(
+      panel.margin.x = unit(.6,"lines"),
+      panel.margin.y = unit(.6,"lines"),
+      panel.border = element_rect(colour = "black", fill = NA),
+      strip.background = element_rect(colour = "black", fill = "grey85"))
+  
+}
+
+theme_soe_foundation <- function(base_size, base_family, use_sizes, facet) {
+  thm <- ggthemes::theme_foundation(base_size = base_size, 
+                                    base_family = base_family, 
+                                    use_sizes = use_sizes)
+  thm <- thm + 
     theme(
       text = element_text(color="black"),
       axis.line = element_line(colour="black"),
@@ -36,31 +65,6 @@ theme_soe <- function(base_size=12, base_family="Verdana", use_sizes=TRUE, facet
       panel.border = element_blank(),
       legend.key = element_blank())
   
-  if (use_sizes) {
-    thm <- thm + theme_foundation_sizes(facet = facet)
-  }
-  
   thm
-}  
-
-#' Soe plot theme for facetted graphs
-#' 
-#' @import ggplot2
-#' @param  base_size base font size (default = 12)
-#' @param  base_family base font family (default = Verdana)
-#' @param  use_sizes use relative font sizes (?)
-#' @export
-#' @keywords plotting theme
-#' @return a ggplot2 theme
-#' @examples \dontrun{
-#' 
-#'}
-theme_soe_facet <- function(base_size=12, base_family="Verdana", use_sizes=TRUE) {
-  theme_soe(facet = TRUE, use_sizes = use_sizes, base_family = base_family) + 
-    theme(
-      panel.margin.x = unit(.6,"lines"),
-      panel.margin.y = unit(.6,"lines"),
-      panel.border = element_rect(colour = "black", fill = NA),
-      strip.background = element_rect(colour = "black", fill = "grey85"))
 }
 
