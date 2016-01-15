3f7c60a95de6e8ee9150344d9926c87e8be36540
Andy Teucher <andy.teucher@gov.bc.ca>
2016-01-15 10:10:32 -0800

    Use complete = TRUE & %+replace% for theme_soe.
    Allows easier theme modifications when called

--- R/theme_soe.r
+++ R/theme_soe.r
@@ -36,10 +36,9 @@ theme_soe <- function(base_size=12, base_family="Verdana") {
 
 theme_soe_facet <- function(base_size = 12, base_family = "Verdana") {
   
-  theme_soe_foundation(base_size = base_size, base_family = base_family) + 
+  theme_soe_foundation(base_size = base_size, base_family = base_family) %+replace%
     theme(
-      panel.margin.x = unit(.6,"lines"),
-      panel.margin.y = unit(.6,"lines"),
+      panel.margin = unit(.6,"lines"),
       panel.border = element_rect(colour = "black", fill = NA),
       strip.background = element_rect(colour = "black", fill = "grey85"))
   
@@ -47,8 +46,7 @@ theme_soe_facet <- function(base_size = 12, base_family = "Verdana") {
 
 theme_soe_foundation <- function(base_size, base_family) {
   thm <- ggthemes::theme_foundation(base_size = base_size, 
-                                    base_family = base_family)
-  thm <- thm + 
+                                    base_family = base_family) +
     theme(
       text = element_text(colour = "black"),
       line = element_line(colour = "black", size = 0.5,
@@ -67,11 +65,11 @@ theme_soe_foundation <- function(base_size, base_family) {
       panel.grid.major = element_line(colour = "grey80",size = 0.5),
       axis.title.y = element_text(vjust = 1, angle = 90),
       axis.title.x = element_text(vjust = 0),
-      panel.margin.x = unit(0, "lines"),
-      panel.margin.y = unit(0, "lines"),
+      panel.margin = unit(0.25, "lines"),
       plot.background = element_blank(),
       panel.border = element_blank(),
-      legend.key = element_blank())
+      legend.key = element_blank(), 
+      complete = TRUE)
   
   thm
 }
