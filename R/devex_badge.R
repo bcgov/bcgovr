1b24fa2e92d3a99c219de2b22ecacee5af4080cb
Andy Teucher <andy.teucher@gov.bc.ca>
2017-02-14 17:03:12 -0800

    Update devex_badge for new location of svgs

--- R/devex_badge.R
+++ R/devex_badge.R
@@ -7,20 +7,26 @@
 #' @export
 devex_badge <- function(project_state, cat = TRUE) {
   project_state <- tolower(project_state)
-  if (project_state == "inspiration") {
-    html <- '<a rel="Inspiration" href="https://github.com/BCDevExchange/docs/blob/master/discussion/projectstates.md"><img alt="An idea being explored and shaped. Open for discussion, but may never go anywhere." style="border-width:0" src="http://bcdevexchange.org/badge/1.svg" title="An idea being explored and shaped. Open for discussion, but may never go anywhere." /></a>'
-  } else if (project_state == "exploration") {
-    html <- '<a rel="Exploration" href="https://github.com/BCDevExchange/docs/blob/master/discussion/projectstates.md"><img alt="Being designed and built, but in the lab. May change, disappear, or be buggy." style="border-width:0" src="http://bcdevexchange.org/badge/2.svg" title="Being designed and built, but in the lab. May change, disappear, or be buggy." /></a>'
-  } else if (project_state == "delivery") {
-    html <- '<a rel="Delivery" href="https://github.com/BCDevExchange/docs/blob/master/discussion/projectstates.md"><img alt="In production, but maybe in Alpha or Beta. Intended to persist and be supported." style="border-width:0" src="http://bcdevexchange.org/badge/3.svg" title="In production, but maybe in Alpha or Beta. Intended to persist and be supported." /></a>'
-  } else {
+  if (!project_state %in% c("inspiration", "exploration", "delivery") || 
+      length(project_state) != 1L) {
     stop("project_state should be one of 'inspiration', 'exploration', or 'delivery'")
   }
-  html <- paste0('<div id="devex-badge">', html, '</div>')
+  html <- make_badge(state = project_state)
   if (!cat) {
     return(html)
   } else {
     cat(html)
     invisible(NULL)
   }
+}
+
+make_badge <- function(state) {
+  title <- tools::toTitleCase(state)
+  state_desc <- c(inspiration = "An idea being explored and shaped. Open for discussion, but may never go anywhere.", 
+                  exploration = "Being designed and built, but in the lab. May change, disappear, or be buggy.", 
+                  delivery = "In production, but maybe in Alpha or Beta. Intended to persist and be supported.")[state]
+  paste0('<div id="devex-badge"><a rel="', title, 
+         '" href="https://github.com/BCDevExchange/docs/blob/master/discussion/projectstates.md"><img alt="', state_desc, 
+         '" style="border-width:0" src="https://assets.bcdevexchange.org/images/badges/', state, 
+         '.svg" title="', state_desc, '" /></a></div>')
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-<a rel="Delivery" href="https://github.com/BCDevExchange/docs/blob/master/discussion/projectstates.md"><img alt="In production, but maybe in Alpha or Beta. Intended to persist and be supported." style="border-width:0" src="http://bcdevexchange.org/badge/3.svg" title="In production, but maybe in Alpha or Beta. Intended to persist and be supported." /></a>
+<a rel="Delivery" href="https://github.com/BCDevExchange/docs/blob/master/discussion/projectstates.md"><img alt="In production, but maybe in Alpha or Beta. Intended to persist and be supported." style="border-width:0" src="https://assets.bcdevexchange.org/images/badges/delivery.svg" title="In production, but maybe in Alpha or Beta. Intended to persist and be supported." /></a>
 
 [![Travis-CI Build Status](https://travis-ci.org/bcgov/envreportutils.svg?branch=master)](https://travis-ci.org/bcgov/envreportutils)
 
