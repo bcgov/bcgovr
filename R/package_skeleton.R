d3c91018c42b552e620bdd5aff3cd7c70d81db77
Sam Albers <sam.albers@gov.bc.ca>
2017-07-05 15:53:10 -0700

    Adding main working package functions

--- R/package_skeleton.R
+++ R/package_skeleton.R
@@ -10,9 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#' Creates the framework of a new analysis development folder
+#' Creates the framework of a new package development folder
 #' 
-#' Creates the folder structure for a new analysis.
 #'
 #' @importFrom git2r repository init
 #'  
@@ -23,7 +22,6 @@
 #' @param git_init Create a new git repository? Logical, default \code{TRUE}.
 #' @param git_clone the url of a git repo to clone.
 #' @param rstudio Create an Rstudio project file?
-#' @param apache Add licensing info for release under Apache 2.0? Default \code{TRUE}.
 #' @param CoC Should a Code of Conduct be added to the repository? Default \code{TRUE}.
 #' @param copyright_holder the name of the copyright holder (default 
 #' "Province of British Columbia). Only necessary if adding a license
@@ -37,61 +35,36 @@
 #' @export
 #' 
 #' @examples \donttest{
-#'  analysis_skeleton(path = "c:/_dev/tarballs", rstudio = TRUE)
+#'  package_skeleton(path = "c:/_dev/tarballs", rstudio = TRUE)
 #' }
-analysis_skeleton <- function(path = ".", git_init = TRUE, git_clone = NULL, 
-                              rstudio = TRUE, apache = TRUE, CoC = TRUE,
+package_skeleton <- function(path = ".", git_init = TRUE, git_clone = NULL, 
+                              rstudio = TRUE, CoC = TRUE,
                               copyright_holder = "Province of British Columbia") {
-
-#   now <- Sys.time()
-#   options(error = quote(error_cleanup(now)))
-#   on.exit(options(error = NULL), add = TRUE)
   
-  if (path != ".") dir.create(path, recursive = TRUE)
+    if (path != ".") dir.create(path, recursive = TRUE)
   
   npath <- normalizePath(path, winslash = "/", mustWork = TRUE)
-
+  
   if (is.character(git_clone)) {
     clone_git(git_clone, npath)
     git_init = FALSE
   }
   
-  ## Need to check for analysis structure
-  Rfiles <- file.path(npath, c("01_load.R", "02_clean.R", "03_analysis.R", "04_output.R", 
-              "internal.R", "run_all.R"))
-  dirs <- file.path(npath, c("R","out", "graphics", "data"))
-  
-  if (any(file.exists(Rfiles, dirs))) { ## file.exists is case-insensitive
-    #if (git) unlink(c(".git", ".gitignore", recursive = TRUE, force = TRUE)
-    stop("It looks as though you already have an analysis set up here!")
-  }
-  
+
   ## Add the necessary R files and directories
-  message("Creating new analysis in ", npath)
-  lapply(Rfiles, file.create)
-  lapply(dirs, dir.create)
+  message("Creating new package in ", npath)
   add_contributing(npath)
   if (CoC) add_code_of_conduct(npath, package = FALSE)
   add_readme(npath, package = FALSE)
   
-  cat('source("01_load.R")
-source("02_clean.R")
-source("03_analysis.R")
-source("04_output.R")
-
-## Make print version
-mon_year <- format(Sys.Date(), "%B%Y")
-outfile <- paste0("envreportbc_[indicator_name]_", mon_year, ".pdf")
-rmarkdown::render("print_ver/[indicator_name].Rmd", output_file = outfile)
-extrafont::embed_fonts(file.path("print_ver/", outfile))
-## You will likely want to "optimize pdf" in Acrobat to make the print version smaller.\n', 
-      file = file.path(npath,"run_all.R"))
+  #if (apache) {
+  #  add_license(npath)
+  #  lapply(Rfiles, add_license_header, year = substr(Sys.Date(), 1, 4), 
+  #         copyright_holder = copyright_holder)
+  #}
   
-  if (apache) {
-    add_license(npath)
-    lapply(Rfiles, add_license_header, year = substr(Sys.Date(), 1, 4), 
-           copyright_holder = copyright_holder)
-  }
+  ## Add in package setup files
+  devtools::setup(rstudio = FALSE)
   
   if (rstudio) {
     if (!length(list.files(npath, pattern = "*.Rproj", ignore.case = TRUE))) {
@@ -100,7 +73,7 @@ extrafont::embed_fonts(file.path("print_ver/", outfile))
       warning("Rproj file already exists, so not adding a new one")
     }
   }
-
+  
   if (git_init) {
     if (file.exists(file.path(npath,".git"))) {
       warning("This directory is already a git repository. Not creating a new one")
@@ -118,10 +91,3 @@ extrafont::embed_fonts(file.path("print_ver/", outfile))
   invisible(npath)
 }
 
-# Function to be executed on error, to clean up files that were created
-# error_cleanup <- function(t) {
-#   info <- file.info(list.files(all.files = TRUE, include.dirs = TRUE, no.. = TRUE))
-#   del_files <- rownames(info[t < info$ctime, ])
-#   cat("Deleting generatedfiles:", del_files)
-#   unlink(del_files, recursive = TRUE, force = TRUE)
-# }
