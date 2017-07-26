624504b2041669921cd5ef1adfbca32dea08a148
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-07-26 15:41:23 -0700

    add package documentation

---
+++ R/bcgovr.R
@@ -0,0 +1,27 @@
+# Copyright {YYYY} {COPYRIGHT_HOLDER}
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+#' bcgovr: A package for helping create project directories that meet BC Government GitHub requirements. 
+#'
+#' @section bcgovr functions:
+#' 
+#'   \itemize{ 
+#'   \item Functions for auto-populating a new R-based data 
+#'    analysis or package project directory
+#'   \item RStudio addins for adding the Apache 2.0 license header 
+#'   and BCDevExchange project state badges
+#'
+#' }
+#'
+#' @docType package
+#' @name bcgovr
+NULL
