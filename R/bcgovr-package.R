22825714981e40712aa9c60f86e66da955be3fca
boshek <sam.albers@gov.bc.ca>
2018-04-26 14:16:57 -0700

    fix document errors in package-level doc

--- R/bcgovr-package.R
+++ R/bcgovr-package.R
@@ -9,9 +9,10 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+
+#' @references
 #'
-#'
-#' @section bcgovr functions:
+#' bcgovr functions:
 #' 
 #'   \itemize{ 
 #'   \item Functions for automating the creation and set up of bcgov R  
@@ -21,8 +22,5 @@
 #'
 #' }
 #'
-#'
-#' @docType package
-#' @name bcgovr
 #' @keywords internal
 "_PACKAGE"
