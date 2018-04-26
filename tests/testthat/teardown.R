910cbddb342c7b48358c3a708321916ef639ce34
Andy Teucher <metoo@abcxyz123.foo>
2018-04-26 16:36:36 -0700

    Add to helper and teardown, add more tests

--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -1,2 +1,5 @@
 usethis::proj_set(original_proj)
 if (tmp_here) unlink(".here")
+setwd(original_wd)
+
+options("bcgovr.coc.email" = coc_email)
--- tests/testthat/test-use.R
+++ tests/testthat/test-use.R
@@ -54,10 +54,8 @@ test_that("use_bcgov_contributing works", {
 
 test_that("use_bcgov_code_of_conduct works", {
   make_test_proj()
-  coc_email <- getOption("bcgovr.coc.email")
-  
+
   # With no email set
-  options("bcgovr.coc.email" = NULL)
   output <- capture.output(use_bcgov_code_of_conduct())
   expect_true(any(grepl("No contact email has been added", output)))
   expect_true(file.exists(proj_file("CODE_OF_CONDUCT.md")))
@@ -72,6 +70,33 @@ test_that("use_bcgov_code_of_conduct works", {
   options("bcgovr.coc.email" = "metoo@gov.bc.ca")
   capture.output(use_bcgov_code_of_conduct())
   expect_true(check_file_contents("metoo@gov.bc.ca", "CODE_OF_CONDUCT.md"))
+})
+
+test_that("use_bcgov_req works", {
+  # orig_wd <- getwd()
+  # setwd(tempdir())
+  # use_bcgov_req()
+  # expect_true(all(file.exists(proj_file(
+  #   c("README.Rmd", "CONTRIBUTING.md", "CODE_OF_CONDUCT.md", "LICENSE")
+  # ))))
+  # setwd(orig_wd)
+  make_test_proj()
+  use_bcgov_req()
+  expect_true(all(file.exists(proj_file(
+    c("README.Rmd", "CONTRIBUTING.md", "CODE_OF_CONDUCT.md", "LICENSE")
+  ))))
+})
+
+test_that("use_bcgov_git works", {
+  dir <- make_test_proj()
+  expect_false(git2r::in_repository(dir))
+  capture.output(use_bcgov_git())
+  expect_true(git2r::in_repository(dir))
   
-  options("bcgovr.coc.email" = coc_email)
+  expect_true(all(file.exists(proj_file(
+    c(".gitignore", "README.Rmd", "CONTRIBUTING.md", "CODE_OF_CONDUCT.md", "LICENSE")
+  ))))
+  
+  git2r::config(user.email = "metoo@abcxyz123.foo")
+  expect_warning(use_bcgov_git(), "You have a non-bcgov email address")
 })
